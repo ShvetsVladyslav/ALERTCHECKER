534d3fb39490cae9b7201340897983b011bd5a96
ShvetsVladyslav <78981819+ShvetsVladyslav@users.noreply.github.com>
2022-11-11 10:29:10 +0200

    Initial commit

---
+++ src/main/java/com/project/alertcheker/AlertchekerApplication.java
@@ -0,0 +1,13 @@
+package com.project.alertcheker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertchekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertchekerApplication.class, args);
+	}
+
+}
